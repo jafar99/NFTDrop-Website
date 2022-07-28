8b6e8f6f8ba4cf30087b19a87feee52cdea7b120
jafar <jafaralihajari@gmail.com>
2022-07-28 21:11:03 +0530

    Nft Minting complited

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+import React, { useEffect, useState } from 'react';
+import { useAddress, useDisconnect, useMetamask, useNFTDrop } from "@thirdweb-dev/react";
 import type { GetServerSideProps, NextPage } from 'next'
 import Image from 'next/image'
 import { disconnect } from 'process';
 import { sanityClient, urlFor } from '../../sanity'
 import { Collection } from '../../typings';
 import Link from 'next/link';
+import { BigNumber } from 'ethers';
+import toast, {Toaster} from 'react-hot-toast'
 
 
 interface Props {
@@ -13,7 +15,14 @@ interface Props {
   }
 
 function NFTDropPage({ collection }: Props) {
-  
+
+  const [claimedSupply,setClaimedSupply]= useState<number>(0);
+  const [totalSupply,setTotalSupply]= useState<BigNumber>();
+  const [priceInEth,setPriceInEth] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const nftDrop = useNFTDrop(collection.address)
+        
+
         // Auth
 
         const connectWithMetamask = useMetamask();
@@ -21,12 +30,111 @@ function NFTDropPage({ collection }: Props) {
         const disconnect = useDisconnect();
         // ----
 
+        useEffect(() => {
+          if (!nftDrop) return
 
-        console.log(address);
+          const fetchPrice = async() => {
+            const claimConditions= await nftDrop.claimConditions.getAll()
+            setPriceInEth(claimConditions?.[0].currencyMetadata.displayValue)
+          }
+          fetchPrice()
+        }, [nftDrop])
+                                                                                    
+
+
+
+        useEffect(() =>{
+
+          if(!nftDrop)return;
+
+          const fetchNFTDropData= async() => { 
+
+            setLoading(true);
+
+            const claimed = await nftDrop.getAllClaimed();
+            const total = await nftDrop.totalSupply();
+         
+            setClaimedSupply(claimed.length);
+            setTotalSupply(total)
+
+            setLoading(false);
+
+        }
+          fetchNFTDropData();
+        },[nftDrop])
+
+
+
+        const mintNft = () => {
+          if (!nftDrop ||! address) return
+          const quantity =1  // how many unique NFTS you want to claim
+
+          setLoading(true)
+
+
+          const Notification = toast.loading('Minting NFT...', {
+            style:{
+              background: 'white',
+              color: 'green',
+              fontWeight: 'bolder',
+              fontSize: '17px',
+              padding: '20px',
+           },
+          })
+            
+
+          nftDrop
+            .claimTo(address,quantity)
+            .then(async (tx) => { 
+              const receipt=tx[0].receipt     // the transaction receipt
+              const claimedTokenId=tx[0].id    // the id of the NFT claimed
+              const claimedNFT=await tx[0].data()   //(optional)get the claimed NFT metadata
+
+
+              toast('HOORAY .. You Successfully Minted!',{
+                duration:8000,
+                 style:{
+                  background:'green',
+                   color:'white',
+                   fontWeight:'bolder',
+                   fontSize:'17px',
+                   padding:'20px',
+                 }
+               })
+
+              console.log(receipt)
+              console.log(claimedTokenId)
+              console.log(claimedNFT)
+            })
+            .catch((err) => {
+              console.log(err)
+
+              toast('Whoops ... Something went wrong!',{
+                style: {
+                  background:'red',
+                  color:'white',
+                  fontWeight:'bolder',
+                  fontSize:'17px',
+                  padding:'20px',
+              }
+            })
+
+            })
+            .finally(() => {
+              setLoading(false)
+              toast.dismiss(Notification)
+            })
+
+       }
+                                            
+                                                        
   
   
     return (
     <div className='flex h-screen flex-col lg:grid lg:grid-cols-10'>
+
+     <Toaster position='bottom-center'/>
+
         {/* left */}
         <div className=" lg:col-span-4 bg-gradient-to-br  from-cyan-800 to-rose-500">
             <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
@@ -77,14 +185,46 @@ function NFTDropPage({ collection }: Props) {
 
 
       <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">  {collection.title} </h1>
+
+      {loading ? (
+
+        <p className="pt-2text-xl text-green-500 animate-pulse"> Loading Supply Count....</p>
+      ):(
+
+        <p className="pt-2text-xl text-green-500"> {claimedSupply} / {totalSupply?.toString()}    NFT's claimed</p>
+
+      )}
+
+   {loading && (
+      <img className='h-80 w-80 object-contain' src="https://cdn.hackernoon.com/images/0*4Gzjgh9Y7Gu8KEtZ.gif"  alt="" />
+   )}
+                             
       
-      <p className="pt-2text-xl text-green-500">13/21 NFT's claimed</p>
+      
   </div>
 
   {/*Mint Button*/}
-  <button  className="mt-10 h-16 w-full rounded-full bg-red-600 text-white font-bold">
+
+  <button onClick={mintNft} disabled={ loading || claimedSupply === totalSupply?.toNumber()|| !address }
+   className="mt-10 h-16 w-full rounded-full bg-red-600 font-bold text-white disabled:bg-gray-400" >
+  
+     {loading ? (
+       <>Loading</>
+     ) :claimedSupply === totalSupply?.toNumber() ? (
+        <>SOLD OUT</>
+     ):! address? (
+       <>Sign in to Mint</>
+     ):(
+      <span>Mint NFT({priceInEth}) ETH</span>
+     )}
+   </button>
+                                           
+
+
+  {/* <button  disabled={loading || claimedSupply === totalSupply?.toNumber() || !address} 
+    className="mt-10 h-16 w-full rounded-full bg-red-600 text-white font-bold  disabled:bg-grey-400">
      Mint NFT(0.01 ETH)
-  </button>
+  </button> */}
                
 
  </div>
